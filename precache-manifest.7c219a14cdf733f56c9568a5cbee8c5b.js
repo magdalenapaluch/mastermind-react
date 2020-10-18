0705eb9e9e5c34e984d6f67033a978d5f42bf16f
magdalenapaluch <1magdalenamuzyk@gmail.com>
2020-10-18 21:03:54 +0200

    Updates

---
+++ precache-manifest.7c219a14cdf733f56c9568a5cbee8c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7bbdd89749aa2029fa35007797cc94",
+    "url": "/mastermind-react/index.html"
+  },
+  {
+    "revision": "9673be5c3cfbcf185b1c",
+    "url": "/mastermind-react/static/css/main.7212affb.chunk.css"
+  },
+  {
+    "revision": "f61cb7e49aa15ba30733",
+    "url": "/mastermind-react/static/js/2.bc27a23e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mastermind-react/static/js/2.bc27a23e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9673be5c3cfbcf185b1c",
+    "url": "/mastermind-react/static/js/main.f0dce242.chunk.js"
+  },
+  {
+    "revision": "4b101ce4a378a31068b4",
+    "url": "/mastermind-react/static/js/runtime-main.83e9b60f.js"
+  }
+]);
